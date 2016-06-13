3ec6d04d7a82c6ced6d59a769b49cfc4404253f1
Dominik Mueller <dominik.mueller@student.hpi.uni-potsdam.de>
2016-06-13 15:42:02 +0200

    LEEA implementation: http://eplex.cs.ucf.edu/papers/morse_gecco16.pdf

---
+++ neuralbench/classification/leea/leeaExperiment.py
@@ -0,0 +1,204 @@
+import copy
+import numpy as np
+import tensorflow as tf
+
+import leeaNetworks
+from neuralbench.classification.dataset.create import dense_to_one_hot
+
+class Gene(object):
+    def __init__(self, chromosome, parent_fitness, evaluate):
+        self.chromosome = chromosome
+        self._evaluate = evaluate
+        self._parent_fitness = parent_fitness
+        self._fitness = None
+
+    @property
+    def fitness(self):
+        return self._fitness
+
+    def calc_fitness(self, X, y, sess):
+        acc, c = self.evaluate(X, y, sess)
+        if c > 10000:
+            print "LOSS is over 10000", c
+
+        # Need a fitness value to maximize
+        own_fitness = 10000.0 - c
+        self._fitness = (1 - PARENT_FITNESS_DECAY) * self._parent_fitness + own_fitness
+        return acc, c
+
+    def evaluate(self, X, y, sess):
+        return self._evaluate(sess, self.chromosome, X, y)
+
+    def mutate(self):
+        child = []
+        for parent_part in self.chromosome:
+            mutation_indices = np.random.uniform(0, 1, parent_part.shape) >= MUTATION_RATE
+            mutations = np.random.uniform(-MUTATION_POWER, MUTATION_POWER, parent_part.shape)
+            mutations[mutation_indices] = 0
+            child.append(parent_part + mutations)
+        return Gene(np.array(child), self.fitness, self._evaluate)
+
+    def mate(self, partner):
+        parent_fitness = (self.fitness + partner.fitness) / 2.0
+        child1, child2 = self._uniform_crossover(partner)
+        new_gene1 = Gene(child1, parent_fitness, self._evaluate)
+        new_gene2 = Gene(child2, parent_fitness, self._evaluate)
+        return new_gene1, new_gene2
+
+    def _uniform_crossover(self, partner):
+        child1 = []
+        child2 = []
+        assert(self.chromosome.shape == partner.chromosome.shape)
+        for parent1, parent2 in zip(self.chromosome, partner.chromosome):
+            assert(parent1.shape == parent2.shape)
+            idx = np.random.uniform(0, 1, parent1.shape) < 0.5
+            c1 = copy.deepcopy(parent1[:])
+            c2 = copy.deepcopy(parent2[:])
+            c1[idx] = parent2[idx]
+            c2[idx] = parent1[idx]
+            child1.append(c1)
+            child2.append(c2)
+
+        return np.array(child1), np.array(child2)
+
+def initialize_population(pop_size, chromosome_template, alpha, evaluation_function):
+    return [Gene(np.array([np.random.uniform(-alpha, alpha, template_part.shape) 
+                            for template_part in chromosome_template])
+                        , 0, evaluation_function) for _ in xrange(pop_size)]
+
+def clac_fitness_population(pop, X, y, sess):
+    for individual in pop:
+        individual.calc_fitness(X, y, sess)
+
+def sort_population(pop):
+    sorted(pop, key=lambda individual: individual.fitness, reverse=True)
+
+def create_new_population(old_pop):
+    pop_size = len(old_pop)
+    new_pop = []
+    while len(new_pop) < pop_size:
+        # Calculate probabilty for each individual
+        fitness_scores = np.array([individual.fitness for individual in old_pop])
+        probs = fitness_scores / sum(fitness_scores)
+
+        if (np.random.uniform(0, 1) < SEXUAL_REPRODUCTION_PROPORTION):
+            parents = np.random.choice(pop_size, 2, replace=False, p=probs)
+            child1, child2 = old_pop[parents[0]].mate(old_pop[parents[1]])
+            new_pop.append(child1)
+            new_pop.append(child2)
+        else:
+            parent = np.random.choice(pop_size, 1, replace=False, p=probs)
+            child = old_pop[parent[0]].mutate()
+            new_pop.append(child)
+
+    global MUTATION_POWER 
+    MUTATION_POWER = MUTATION_POWER * MUTATION_POWER_DECAY
+
+    # Cut offspring besides population_size
+    return new_pop[:pop_size]
+
+def configure_for_training(params_string, max_evaluations, n_classes, eval_net, weights_template, seed, f):
+
+    def train_network(X_train, y_train, X_validate, y_validate, X_test, y_test, test_split=0, validate_split=0):
+        file_start = "%d\t%d\t%d" % (seed, test_split, validate_split)
+
+        assert(n_classes == len(np.unique(y_train)))
+        y_train_one_hot = dense_to_one_hot(y_train, n_classes)
+        assert(n_classes == len(np.unique(y_validate)))
+        y_validate = dense_to_one_hot(y_validate, n_classes)
+        assert(n_classes == len(np.unique(y_test)))
+        y_test = dense_to_one_hot(y_test, n_classes)
+
+        pop = initialize_population(population_size, weights_template, INITIAL_WEIGHT_RANGE, eval_net)
+
+        with tf.Session() as sess:
+            validate_results = np.array([[acc, cost] for acc, cost in [individual.evaluate(X_validate, y_validate, sess) for individual in pop]])
+            best_validation_acc_index = np.argmax(validate_results, axis=0)[0]
+            acc, cost = pop[best_validation_acc_index].evaluate(X_test, y_test, sess)
+
+            f.write("%s\t%s\t%s\t%d\t%s\t%f\n" % (file_start, params_string, "val", 0, "acc", np.max(validate_results, axis=0)[0]))
+            f.write("%s\t%s\t%s\t%d\t%s\t%f\n" % (file_start, params_string, "test", 0, "acc", acc))
+            f.write("%s\t%s\t%s\t%d\t%s\t%f\n" % (file_start, params_string, "val", 0, "cost", np.min(validate_results, axis=0)[1]))
+            f.write("%s\t%s\t%s\t%d\t%s\t%f\n" % (file_start, params_string, "test", 0, "cost", cost))
+
+            evaluations_per_generation = population_size * batch_size
+            num_generations = max_evaluations/(evaluations_per_generation) + 1
+
+            # Used to sample a batch with same class ratios
+            from sklearn.cross_validation import StratifiedShuffleSplit
+            sss = StratifiedShuffleSplit(y_train.reshape(-1), num_generations, train_size=batch_size, random_state=seed)
+            
+            for generation, (batch_index, _) in enumerate(sss):
+                X_current = X_train[batch_index]
+                y_current = y_train_one_hot[batch_index]
+
+                train_results = np.array([[acc, cost] for acc, cost in [individual.calc_fitness(X_current, y_current, sess) for individual in pop]])
+
+                sort_population(pop)
+                pop = create_new_population(pop)
+
+                validate_results = np.array([[acc, cost] for acc, cost in [individual.evaluate(X_validate, y_validate, sess) for individual in pop]])
+                best_validation_acc_index = np.argmax(validate_results, axis=0)[0]
+                acc, cost = pop[best_validation_acc_index].evaluate(X_test, y_test, sess)
+                
+                train_evaluations = (generation + 1) * evaluations_per_generation
+
+                f.write("%s\t%s\t%s\t%d\t%s\t%f\n" % (file_start, params_string, "train", train_evaluations, "acc", np.max(train_results, axis=0)[0]))
+                f.write("%s\t%s\t%s\t%d\t%s\t%f\n" % (file_start, params_string, "val", train_evaluations, "acc", np.max(validate_results, axis=0)[0]))
+                f.write("%s\t%s\t%s\t%d\t%s\t%f\n" % (file_start, params_string, "test", train_evaluations, "acc", acc))
+                f.write("%s\t%s\t%s\t%d\t%s\t%f\n" % (file_start, params_string, "train", train_evaluations, "cost", np.min(train_results, axis=0)[1]))
+                f.write("%s\t%s\t%s\t%d\t%s\t%f\n" % (file_start, params_string, "val", train_evaluations, "cost", np.min(validate_results, axis=0)[1]))
+                f.write("%s\t%s\t%s\t%d\t%s\t%f\n" % (file_start, params_string, "test", train_evaluations, "cost", cost))
+                if generation % 100 == 0:
+                    f.flush()
+
+    return train_network
+
+if __name__ == '__main__':
+    print "Starting parameter sweep for leea"
+    import sys
+    from tqdm import *
+    from neuralbench.classification.dataset.create import createDataSet, run_test_validate_splits
+    
+    dataset_name = "spiral"
+    architecture = "deep"
+
+    # for dataset_name in tqdm(["spiral", "circle"]):
+    #     for architecture in tqdm(["perceptron", "small", "big", "deep"]):
+    
+    X_train, y_train, X_test, y_test = createDataSet(dataset_name)
+    eval_net, weights_template = leeaNetworks.createArchitecture(architecture, dataset_name)
+
+    PARENT_FITNESS_DECAY = 0.2
+    STARTING_MUTATION_POWER = 0.03
+    MUTATION_POWER = 0.03
+    MUTATION_RATE = 0.4
+
+    # POPULATION_SIZE = 1000
+    SELECTION_PROPORTION = 0.4
+    MUTATION_POWER_DECAY = 0.99
+    SEXUAL_REPRODUCTION_PROPORTION = 0.5
+    
+    INITIAL_WEIGHT_RANGE = 4
+    seed = 0
+    file_identifier = str(sys.argv[1]) + "/params_leea_%s_%s_%03d_%.2f_%.2f_%.2f_%.1f" % (architecture, dataset_name, seed,
+                                MUTATION_POWER, MUTATION_RATE, SELECTION_PROPORTION, INITIAL_WEIGHT_RANGE)
+    file_name = "%s.dat" % (file_identifier)
+    f = open(file_name, 'w')
+    f.write("seed\ttest_split\tvalidation_split")
+    f.write("\tpopulation_size\tmutation_power\tmutation_rate\tselection_proportion\tbatch_size\tinitial_weight_range")
+    f.write("\tevaluation_data\tevaluations\tfitness_type\tresult\n")
+
+    for population_size in tqdm([10, 40, 100, 400, 1000]):
+        for batch_size in tqdm([10, 20, 40, 80, 100, 150, 200, 250]):
+            standard_params = "%d\t%0.2f\t%0.2f\t%0.1f\t%d\t%0.1f" % (population_size, MUTATION_POWER, MUTATION_RATE,
+                    SELECTION_PROPORTION, batch_size, INITIAL_WEIGHT_RANGE)
+
+            # configure_for_training(params, max_evaluations, n_classes, eval_genotype, num_network_weights, seed, file_identifier)
+            train_network = configure_for_training(standard_params, 300000, 2, eval_net, weights_template, seed, f)
+            run_test_validate_splits(train_network, X_train, y_train, folds=10)
+
+    f.close()
+
+
+
---
+++ neuralbench/classification/leea/leeaNetworks.py
@@ -0,0 +1,121 @@
+import itertools
+import tensorflow as tf
+import numpy as np
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return itertools.izip(a, b)
+
+def build_perceptron_network(net_architecture):
+    assert(len(net_architecture) == 2)
+    y = tf.placeholder(tf.float64, [None, net_architecture[-1]])
+    x = tf.placeholder(tf.float64, [None, net_architecture[0]])
+
+
+    print "Creating w0 and b0, with a: %d and b: %d" % (net_architecture[0], net_architecture[1])
+    w0 = tf.placeholder(tf.float64, [net_architecture[0], net_architecture[1]])
+    b0 = tf.placeholder(tf.float64, [net_architecture[1]])
+
+    print "Creating pred with (x * w0 + b0)"
+    pred = tf.add(tf.matmul(x, w0), b0)
+
+    # Define loss and accuracy
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+    def runNetwork(sess, genotype, cur_data, cur_label):
+        feed_dict = {x: cur_data, y: cur_label}
+        feed_dict[w0] = genotype[0][:-1]
+        feed_dict[b0] = genotype[0][-1:].reshape(net_architecture[1])
+
+        acc, c = sess.run([accuracy, cost], feed_dict=feed_dict)
+        return acc, c
+
+    genotype_shape = np.array([ np.arange(a*b + b).reshape((a+1,b)) for (a, b) in pairwise(net_architecture)])
+    return runNetwork, genotype_shape
+
+
+def build_multilayer_network(net_architecture):
+    y = tf.placeholder(tf.float64, [None, net_architecture[-1]])
+    x = tf.placeholder(tf.float64, [None, net_architecture[0]])
+
+    layers = {}
+    for i, (a, b) in enumerate(pairwise(net_architecture)):
+        print "Creating w%d and b%d, with a: %d and b: %d" % (i, i, a, b)
+        layers["w%d" % i] = tf.placeholder(tf.float64, [a, b])
+        layers["b%d" % i] = tf.placeholder(tf.float64, [b])
+
+    # tanh((x * W_0) + b_0)
+    print "Creating l0 with tanh(x * w0 + b0)"
+    layers["l0"] = tf.nn.tanh(tf.add(tf.matmul(x, layers["w0"]), layers["b0"]))
+
+    hidden_layer = len(net_architecture) - 2
+
+    for i in xrange(1, (hidden_layer)):
+        # tanh((l_i-1 * W_i) + b_i)
+        last_layer = i - 1
+        print "Creating l%d with tanh(l%d * w%d + b%d)" % (i, last_layer, i, i)
+        layers["l%d" % i] = tf.nn.tanh(tf.add(tf.matmul(layers["l%d" % last_layer], layers["w%d" % i]), layers["b%d" % i]))
+
+    print "Creating pred with (l%d * w%d + b%d)" % (hidden_layer-1, hidden_layer, hidden_layer)
+    pred = tf.add(tf.matmul(layers["l%d" % (hidden_layer-1)], layers["w%d" % hidden_layer]), layers["b%d" % hidden_layer])
+
+    # Define loss and accuracy
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+    def runNetwork(sess, genotype, cur_data, cur_label):
+        feed_dict = {x: cur_data, y: cur_label}
+        for i, n_nodes in enumerate(net_architecture[1:]):
+            # print genotype[i].shape, net_architecture[i], n_nodes
+            assert(genotype[i].shape[0] == net_architecture[i] + 1)
+            assert(genotype[i].shape[1] == n_nodes)
+            feed_dict[layers["w%d" % i]] = genotype[i][:-1]
+            feed_dict[layers["b%d" % i]] = genotype[i][-1:].reshape(n_nodes)
+
+        acc, c = sess.run([accuracy, cost], feed_dict=feed_dict)
+        return acc, c
+
+    genotype_shape = np.array([ np.arange(a*b + b, dtype=np.float64).reshape((a+1,b)) for (a, b) in pairwise(net_architecture)])
+    return runNetwork, genotype_shape
+
+def createArchitecture(choice, dataset_name):
+    if choice == "perceptron":
+        if dataset_name == "mnist":
+            return build_perceptron_network([784, 10])
+        else:
+            return build_perceptron_network([2, 2])
+    elif choice == "small":
+        if dataset_name == "mnist":
+            return build_multilayer_network([784, 80, 10])
+        else:
+            return build_multilayer_network([2, 5, 2])
+    elif choice == "big":
+        if dataset_name == "mnist":
+            return build_multilayer_network([784, 400, 10])
+        else:
+            return build_multilayer_network([2, 10, 2])
+    elif choice == "deep":
+        if dataset_name == "mnist":
+            return build_multilayer_network([784, 40, 30, 20, 10])
+        else:
+            return build_multilayer_network([2, 8, 8, 4, 4, 2])
+    else:
+        print "Bad luck no known architecture"
+
+
+if __name__ == '__main__':
+    arch = [4, 2, 2, 3]
+    evalNet, weights = build_multilayer_network(arch)
+    # for w in weights:
+        # print w.shape
+
+    with tf.Session() as sess:
+        cur_data = np.arange(arch[0]*4).reshape((4,arch[0]))
+        cur_label = np.arange(arch[-1]*4).reshape((4,arch[-1]))
+
+        print evalNet(sess, weights, cur_data, cur_label)
